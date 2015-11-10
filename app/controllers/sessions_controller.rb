6101a79dc089fef5b707ba1b9bdc42d0fe1da507
Pablo Ortega Mesa <portega@modyo.com>
2015-11-10 11:53:42 -0300

    Removing .idea from index project.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,3 @@
-# require 'twitter_client'
 class SessionsController < ApplicationController
   def new
   end
@@ -21,30 +20,17 @@ class SessionsController < ApplicationController
     # render :text => "You can login using #{auth_hash["provider"].capitalize} too!"
 
     if auth_hash["provider"] == "twitter"
-      logger.info "Token: " + auth_hash["credentials"]["token"]
-      logger.info "Secret: " + auth_hash["credentials"]["secret"]
-      config = {
-          consumer_key:    auth_hash["credentials"]["token"],
-          consumer_secret: auth_hash["credentials"]["secret"],
-      }
-
-      @client = ::Twitter::REST::Client.new do |config|
-        config.consumer_key = "KXt2vqosFVQZdvXievvcXXgO8"
-        config.consumer_secret = "nRX6rDSoLbHaDiuBRfGI21C1Qqn6loilytbwa66GXekL89HDKI"
-        config.access_token = "31307687-6F47KlrUgbZwyCL8jfK2nJkqx3oYbyjIEVD8rGFVl"
-        config.access_token_secret = "OyLlqR7OYLlLk1MUqcQgkqd5QrGAnZcXhjTQJRxJZDrGY"
-      end
-
       screen_names = []
       next_cursor = -1
+
       while next_cursor != 0
-        cursor = @client.followers(:cursor => next_cursor)
+        cursor = $twitter.followers(cursor: next_cursor, count: 200)
         cursor.each do |follower|
           ap follower.screen_name
           screen_names.append(follower.screen_name)
         end
-        next_cursor = cursor.next_cursor
-        sleep(2000)
+        next_cursor = cursor.send(:next_cursor)
+        logger.info next_cursor
       end
 
       render :text => "# Followers indexed: #{screen_names.count}"
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,5 @@
-twitter_client = Twitter::REST::Client.new do |config|
+
+$twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = "KXt2vqosFVQZdvXievvcXXgO8"
   config.consumer_secret = "nRX6rDSoLbHaDiuBRfGI21C1Qqn6loilytbwa66GXekL89HDKI"
   config.access_token = "31307687-6F47KlrUgbZwyCL8jfK2nJkqx3oYbyjIEVD8rGFVl"
